c364570cf7ad64070abadc8975af4ca5d2816f5d
John Baker <bakerjd99@gmail.com>
2018-02-23 08:56:53 -0700

    fix typos

--- notebooks/changekeywords.py
+++ notebooks/changekeywords.py
@@ -1,6 +1,6 @@
 # Scan CSV changes files in local directories and apply keyword changes.
-# keyword changes. Run after refreshing manifest files and updating
-# keyword changes files: assumes (smugpyter, printkeys) is on sys.path
+# Run after refreshing manifest files and updating keyword changes files: 
+# assumes (smugpyter, printkeys) is on sys.path
 import printkeys
 
 pk = printkeys.PrintKeys()
